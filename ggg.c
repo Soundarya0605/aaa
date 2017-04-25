752b590b61a9505db3b0050f2848ed294cfffb1b
Soundarya0605 <soundaryamurugan1997@gmail.com>
2017-04-25 11:04:51 +0530

    Create ggg.c

---
+++ ggg.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+int a,b,c;
+clrscr();
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if((a>b)&&(a>c))
+printf("%d is greater,a);
+elseif((b>c)&&(b>a))
+printf("%d is greater,b);
+else
+printf("%d is greater,c);
+getch();
+return 0;
+}
