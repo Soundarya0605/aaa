3a1c4262eeeee019630ed10ac05e8b8a990ad353
Soundarya0605 <soundaryamurugan1997@gmail.com>
2017-04-25 12:13:18 +0530

    Create ttt.c

---
+++ ttt.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,i,sum=0;
+scanf("%d",&a);
+    {
+        
+    for(i=0;i<=a;i++)
+    {
+      sum=sum+i;
+    }
+    }
+ printf("%d",sum);
+    return 0;
+    }
